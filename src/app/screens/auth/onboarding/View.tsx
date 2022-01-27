327e53913fcae25aa96a82a7c20aa6aafad2a540
Atawodi Emmanuel <gmemmy22@gmail.com>
2022-01-27 14:52:09 +0100

    chore: setup linting and pre-commit hooks

---
+++ src/app/screens/auth/onboarding/View.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export function Onboarding() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Text>Hello motherfuckers</Text>
+    </View>
+  )
+}
